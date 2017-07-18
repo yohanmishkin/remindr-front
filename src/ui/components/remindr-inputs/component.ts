0b06cda667fed489ab1f71e5752a26ee30768890
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-07-17 21:45:35 -0400

    Style and stuff

--- src/ui/components/remindr-inputs/component.ts
+++ src/ui/components/remindr-inputs/component.ts
@@ -3,7 +3,7 @@ import Component, { tracked } from '@glimmer/component';
 export default class RemindrInputs extends Component {
   @tracked current = 0;
   questions = [
-    'What do you want to be reminded of?',
+    'What are you trying not to forget?',
     'When do you want to be reminded?',
     'What phone number should we call?'
   ];
@@ -11,6 +11,11 @@ export default class RemindrInputs extends Component {
   @tracked('current')
   get currentQuestion() {
     return this.questions[this.current];
+  }
+
+  @tracked('current')
+  get questionNumber() {
+    return this.current + 1;
   }
 
   next() {
